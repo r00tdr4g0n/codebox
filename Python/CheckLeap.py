b2858b4afdada040b616ebaf9389225db43f5fbc
BOOK-36O9DT5K00\wonyong <r00tdr4g0n@gmail.com>
2024-05-28 22:57:47 +0900

    [add] CheckLeap

---
+++ Python/CheckLeap.py
@@ -0,0 +1,11 @@
+def CheckLeap(a_year: int) -> bool:
+    if a_year % 4 == 0:
+        if a_year % 100 == 0:
+            if a_year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
