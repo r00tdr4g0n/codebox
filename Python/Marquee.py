e39ccbd1bd21f954580ea6c45c04037701216543
BOOK-36O9DT5K00\wonyong <r00tdr4g0n@gmail.com>
2024-05-28 23:19:56 +0900

    [add] Marquee

---
+++ Python/Marquee.py
@@ -0,0 +1,29 @@
+import threading
+import time
+
+__isWorking: bool = True
+
+def Marquee(a_str: str):
+    global __isWorking
+    
+    __isWorking = True
+    
+    ch: list = ['\\', '|', '/', '-']
+    idx: int = 0
+    
+    while __isWorking:
+        print(f"{a_str} {ch[idx]}", end='\r')
+        idx += 1
+        idx %= len(ch)
+        time.sleep(0.1)
+        
+def MarqueeTest():
+    global __isWorking
+    
+    marqueeThread = threading.Thread(target=Marquee, args=("Connecting...", ))
+    marqueeThread.start()
+    time.sleep(3)
+    __isWorking = False
+    
+    
+    
